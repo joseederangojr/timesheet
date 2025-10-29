f8816c30b9ecc6c106549cf3e5379dcb9157a4bd
Jose Ederango Jr <joseederangojr@gmail.com>
2025-10-29 16:03:31 +0800

    feat: Update admin users functionality with flash message support
    - Update UsersController to use new status format for user creation/deletion messages
    - Add FlashStatusMessage component to admin users pages
    - Update CreateUserTest to include sidebar cookie initialization
    - Update UsersControllerTest to expect new status format

--- resources/js/pages/admin/users/index.tsx
+++ resources/js/pages/admin/users/index.tsx
@@ -1,10 +1,12 @@
 import { AdminLayout } from '@/components/layouts/admin-layout';
+import { FlashStatusMessage } from '@/components/ui/flash-status-message';
 import { AdminUsersDataTable, AdminUsersHeader } from './components';
 
 export default function AdminUsersIndex() {
     return (
         <AdminLayout currentPath="admin/users">
             <div className="space-y-6">
+                <FlashStatusMessage />
                 <AdminUsersHeader />
                 <AdminUsersDataTable />
             </div>
